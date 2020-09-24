cee457d9776bac991347571bfd020ab0fddad621
77283103 <www_xiaoxuzi_xhb@163.com>
2020-09-24 14:35:36 +0800

    范本模块，依据模块 跟新

--- blade-service-api/blade-contract-api/src/main/java/org/springblade/contract/vo/AccordingResponseVO.java
+++ blade-service-api/blade-contract-api/src/main/java/org/springblade/contract/vo/AccordingResponseVO.java
@@ -1,6 +1,7 @@
 package org.springblade.contract.vo;
 
 import lombok.*;
+import org.springblade.contract.entity.AccordingEntity;
 import org.springblade.core.mp.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
 
@@ -16,7 +17,7 @@ import io.swagger.annotations.ApiModel;
 @AllArgsConstructor
 @ApiModel(description = "合同依据管理返回对象")
 @EqualsAndHashCode(callSuper = true)
-public class AccordingResponseVO extends BaseEntity {
+public class AccordingResponseVO extends AccordingEntity {
 
 	private static final long serialVersionUID = 1L;
 
