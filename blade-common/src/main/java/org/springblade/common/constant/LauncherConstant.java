e911b2fc749c24181017d2da905826a65ebe208d
liumeng <liumeng>
2020-10-27 18:20:34 +0800

    合同表单配置

--- blade-common/src/main/java/org/springblade/common/constant/LauncherConstant.java
+++ blade-common/src/main/java/org/springblade/common/constant/LauncherConstant.java
@@ -40,7 +40,7 @@ public interface LauncherConstant {
 	/**
 	 * nacos dev 地址
 	 */
-	String NACOS_DEV_ADDR = "10.103.106.9:8848";
+	String NACOS_DEV_ADDR = "127.0.0.1:8848";
 
 	/**
 	 * nacos prod 地址
