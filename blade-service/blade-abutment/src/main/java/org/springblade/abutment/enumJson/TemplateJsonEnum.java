02d39b780e4e65c67432d5af50c6d8ab928a1497
xhb <outlookme@163.com>
2021-08-02 12:26:17 +0800

    模板公共字段枚举类

--- blade-service/blade-abutment/src/main/java/org/springblade/abutment/enumJson/TemplateJsonEnum.java
+++ blade-service/blade-abutment/src/main/java/org/springblade/abutment/enumJson/TemplateJsonEnum.java
@@ -29,7 +29,550 @@ import java.util.stream.Stream;
 @AllArgsConstructor
 @Slf4j
 public enum TemplateJsonEnum {
+	// 物流服务合同（二段仓储+配送）
+	WLFW_23("WLFW_23") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
 
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 作业外包协议
+	WBXY_27("WBXY_27") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 新陈列协议书
+	CLXY_42("CLXY_42") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 店招合同
+	DZHT_35("DZHT_35") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 媒体类：视频广告改编合同
+	GBHT_14("GBHT_14") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 加工承揽合同（代工合同）
+	CLHT_19("CLHT_19") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 音频制作合同
+	ZZHT_18("ZZHT_18") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 修图合同
+	XTHT_17("XTHT_17") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 视频制作合同
+	ZZHT_16("ZZHT_16") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 视频广告拍摄制作合同
+	ZZHT_15("ZZHT_15") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 活动执行合同
+	HDZX_05("HDZX_05") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 物流服务合同（二段配送）
+	FWHT_24("FWHT_24") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 媒体类：平面广告拍摄制作合同
+	ZZHT_12("ZZHT_12") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 生产类：设备维修保养合同
+	BYHT_21("BYHT_21") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 物流服务合同（一段+调拨运输）
+	FWHT_25("FWHT_25") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 采购类_打样合同书
+	DYHT_03("DYHT_03") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 保密协议（二方）
+	BMXY_01("BMXY_01") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 保密协议（三方）
+	BMXY_02("BMXY_02") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 房屋租赁合同模板
+	FWZL_36("FWZL_36") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 消防设施维修保养服务合同
+	XFWB_60("XFWB_60") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 广告制作安装合同模板
+	GGZZ_04("GGZZ_04") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 买卖合同（国内设备购买）
+	MMHT_06("MMHT_06") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	// 2021年统一e商城平台入驻服务协议（统一经销商）
+	FWXY_50("FWXY_50") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//统一e商城平台入驻服务协议（统一经销商）目前使用模板
+	FWXY_29("FWXY_29") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//物流服务合同（冷冻）
+	WLFW_59("WLFW_59") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//劳务派遣合同
+	LWHT_52("LWHT_52") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//市调合同（定性+定量）
+	SDHT_13("SDHT_13") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//班车服务合同
+	FWHT_51("FWHT_51") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//设备投放使用协议
+	SBTF_40("SBTF_40") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//生产项目外包服务合同
+	FWHT_22("FWHT_22") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//配送服务合同
+	FWHT_38("FWHT_38") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
+	//下脚品买卖合同
+	MMHT_26("MMHT_26") {
+		@Override
+		public String setScheduler() {
+			return null;
+		}
+
+		@Override
+		public Object setScheduler(String type) {
+			return null;
+		}
+
+		@Override
+		public List<ConcurrentHashMap<Object, Object>> setScheduler(String type, Boolean tag) {
+			return null;
+		}
+	},
 	//原物料-买卖合同
 	MMHT_10("MMHT_10") {
 		@Override
